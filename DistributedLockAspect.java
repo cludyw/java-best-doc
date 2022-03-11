87c5875571c9afd7eab669bb763f6809e1000d53
cludyw <51012037+cludyw@users.noreply.github.com>
2022-03-11 17:28:49 +0800

    Update DistributedLockAspect.java

--- DistributedLockAspect.java
+++ DistributedLockAspect.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2006-2021 Hzins Ltd. All Rights Reserved.
- * <p>
- * This code is the confidential and proprietary information of
- * Hzins.You shall not disclose such Confidential Information
- * and shall use it only in accordance with the terms of the agreements
- * you entered into with Hzins,https://www.huize.com.
- * </p>
- */
-package com.hzins.travel.byt.starter.redis.lock;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -18,17 +7,13 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-/**
- * @author hz21056680
- * @date 2021/11/12 12:32
- */
 @Component
 @Aspect
 public class DistributedLockAspect {
     @Autowired
     RedissonClient redissonClient;
 
-    @Pointcut("@annotation(com.hzins.travel.byt.starter.redis.lock.DistributedLock) && within(com.hzins.travel..*)")
+    @Pointcut("@annotation(lock.DistributedLock) && within(com..*)")
     public void pointcut() {}
 
     @Around("pointcut()")
